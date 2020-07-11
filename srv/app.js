493690ad7445be8805a03d2d76498fe8bfa5abd2
clayton <clayton@trilliumtransit.com>
2020-07-11 12:42:32 -0700

    iphone support?

--- srv/app.js
+++ srv/app.js
@@ -1,5 +1,5 @@
 const NodeMediaServer = require('node-media-server');
- 
+
 const config = {
   rtmp: {
     port: 1935,
@@ -10,9 +10,22 @@ const config = {
   },
   http: {
     port: 8000,
+    mediaroot: './media',
     allow_origin: '*'
+  },
+  trans: {
+    ffmpeg: '/usr/local/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
   }
 };
- 
+
 var nms = new NodeMediaServer(config)
 nms.run();
