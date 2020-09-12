62b65f1951d5ca2d9fa2110efc227eb8a0d20343
arr0nax <claytonccollins@gmail.com>
2020-09-12 02:15:12 +0000

    port

--- chat/index.js
+++ chat/index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3004;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
