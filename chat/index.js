a39b3e7d06ccaa0393d81aa3b6960f6bbf88e0cf
clayton <clayton@trilliumtransit.com>
2020-09-11 19:15:27 -0700

    names

--- chat/index.js
+++ chat/index.js
@@ -2,14 +2,20 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var rug = require('random-username-generator');
+rug.setSeperator('_');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('request name', function() {
+    console.log('request recieved');
+    socket.emit('send name', rug.generate());
+  });
+  socket.on('chat message', function(message) {
+    io.emit('chat message', message);
   });
 });
 
