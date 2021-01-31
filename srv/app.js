c9db7c7f51b23a53d81a78e17ca73f5144c152c1
clayton <clayton@trilliumtransit.com>
2021-01-31 12:32:29 -0800

    working with obs

--- srv/app.js
+++ srv/app.js
@@ -19,9 +19,7 @@ const config = {
       {
         app: 'live',
         hls: true,
-        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-        dash: true,
-        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]'
       }
     ]
   }
