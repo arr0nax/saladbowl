81d15cff8968bd683f724ad5c43fd1555c6d7fb2
arr0nax <claytonccollins@gmail.com>
2021-11-28 20:19:46 +0000

    yup

--- daily-tv-playlist.sh
+++ daily-tv-playlist.sh
@@ -5,5 +5,6 @@
 scp /home/flock/saladbowl/today.html flock@saladbowl.zone:/var/www/saladbowl/
 scp /home/flock/saladbowl/today.json flock@saladbowl.zone:/var/www/saladbowl/
 sleep 1
+#/home/flock/saladbowl/tv/ffplayout-engine/ffplayout.py -c /home/flock/saladbowl/tv/ffplayout-engine/ffplayout.yml -l none -p /home/flock/saladbowl/test.json -s now -t none
 /home/flock/saladbowl/tv/ffplayout-engine/ffplayout.py -c /home/flock/saladbowl/tv/ffplayout-engine/ffplayout.yml -l none -p /home/flock/movies/`date "+%Y"`/`date "+%m"`/`date "+%F"`.json -s now -t none
 
