8f8bbad06d312939d507a1dd40595a92cdd8b5aa
arr0nax <claytonccollins@gmail.com>
2021-11-28 19:10:43 +0000

    store

---
+++ daily-tv-playlist.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+/home/flock/saladbowl/tv/playlist-generator/generate-playlist.py -c /home/flock/saladbowl/tv/ffplayout-engine/ffplayout.yml -i /home/flock/movies -o /home/flock/movies
+/usr/bin/python3 /home/flock/saladbowl/create-schedule.py
+scp /home/flock/saladbowl/today.html flock@saladbowl.zone:/var/www/saladbowl/
+scp /home/flock/saladbowl/today.json flock@saladbowl.zone:/var/www/saladbowl/
+sleep 1
+/home/flock/saladbowl/tv/ffplayout-engine/ffplayout.py -c /home/flock/saladbowl/tv/ffplayout-engine/ffplayout.yml -l none -p /home/flock/movies/`date "+%Y"`/`date "+%m"`/`date "+%F"`.json -s now -t none
+
