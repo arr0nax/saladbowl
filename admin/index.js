fe27c22398420a1115e2c6f0462733792707daf5
clayton <clayton@trilliumtransit.com>
2021-12-28 14:56:21 -0800

    save for tomorrow;

--- admin/index.js
+++ admin/index.js
@@ -5,6 +5,12 @@ var app = express();
 var http = require('http').Server(app);
 app.use(express.json());
 
+Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -26,7 +32,12 @@ app.get('/library.json', function(req, res, next) {
 });
 
 app.post('/save-schedule', function(req, res, next) {
-	fs.writeFile('filename.json', JSON.stringify(req.body), (err) => {
+	let current_date = new Date();
+	let date_time = current_date.addDays(10);
+	let date = ("0" + date_time.getDate()).slice(-2);
+	let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+	let year = date_time.getFullYear();
+	fs.writeFile(year + "-" + month + "-" + date + '.json', JSON.stringify(req.body), (err) => {
 		if (err) return console.log(err);
 	});
 	res.send('success')
